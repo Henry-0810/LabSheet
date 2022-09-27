10d256645e72372c062b0814da739f3482d8b250
t00229172 <Mun.Li.Pan@students.ittralee.ie>
2022-09-27 15:24:02 +0100

    exercise 4 done

--- LabSheet/LabSheet4/exercise4/TestBook.java
+++ LabSheet/LabSheet4/exercise4/TestBook.java
@@ -3,6 +3,7 @@ package LabSheet4.exercise4;
 import LabSheet4.exercise3.Book;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TestBook {
     public static void main(String[] args) {
@@ -11,7 +12,12 @@ public class TestBook {
         double price;
         String ISBN;
         int pages;
+        String output = "";
 
+        JTextArea textarea = new JTextArea();
+        Font font = new Font("monospaced",Font.PLAIN,12);
+        textarea.setFont(font);
+        output += String.format("%-25s%-9s%-15s%s","Title","Price","ISBN","Pages") + "\n\n";
         //Favourite book inputs
         title = JOptionPane.showInputDialog("Please enter the title of your favourite book");
         price = Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of your favourite book"));
@@ -19,6 +25,8 @@ public class TestBook {
         pages = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of pages of your favourite book"));
 
         Book book1 = new Book(title,price,ISBN,pages);
+        output += String.format("%-25s%-9s%-15s%s",title,price,ISBN,pages) + "\n\nh";
+
         //Least favourite book inputs
         title = JOptionPane.showInputDialog("Please enter the title of your least favourite book");
         price = Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of your least favourite book"));
@@ -26,12 +34,11 @@ public class TestBook {
         pages = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of pages of your least favourite book"));
 
         Book book2 = new Book(title,price,ISBN,pages);
+        output += String.format("%-25s%-9s%-15s%s",title,price,ISBN,pages);
 
-        String output = "Calling the no-argument Book constructor. The first Book object details are: \n\n"+
-                book1 + "\n\nCalling the no-argument Book constructor. The second Book object details are: \n\n"+
-                book2;
+        textarea.append(output);
 
-        JOptionPane.showMessageDialog(null,output,"Book Object Data",1);
+        JOptionPane.showMessageDialog(null,textarea,"Book Object Data",1);
     }
 
 }
